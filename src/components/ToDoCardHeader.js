e7e7639de96a79b76b082d69c336026a6ab0118f
Will Adams <bushblade@gmail.com>
2018-10-21 21:41:10 +0100

    edit a header title

--- src/components/ToDoCardHeader.js
+++ src/components/ToDoCardHeader.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class ToDoCardHeader extends Component {
+  componentDidMount() {
+    const { id } = this.props
+    this.refs[`header${id}`].focus()
+  }
+
   render() {
     const { id, deleteCard, updateTitle, title } = this.props
     return (
       <header className="card-header">
         <p
           className="card-header-title"
+          ref={`header${id}`}
           contentEditable
           suppressContentEditableWarning
-          onBlur={({ target: { textContent } }) => updateTitle(id, textContent)}>
+          onFocus={e => {
+            this.refs[`header${id}`].classList.add('selected-header')
+          }}
+          onBlur={({ target: { textContent } }) => {
+            updateTitle(id, textContent)
+            this.refs[`header${id}`].classList.remove('selected-header')
+          }}
+          onKeyDown={({ keyCode }) => {
+            if (keyCode === 13) {
+              this.refs[`header${id}`].blur()
+            }
+          }}>
           {title}
         </p>
-        <span className="card-header-icon" onClick={() => deleteCard(id)}>
+        <span
+          className="card-header-icon tooltip"
+          onClick={() => deleteCard(id)}
+          data-tooltip="Delete card">
           <span className="icon has-text-dark">
             <i className="fas fa-trash" />
           </span>
